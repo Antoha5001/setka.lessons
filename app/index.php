ea924410cd3198bc3f82acbfed8b07125bdb5c0c
Antoha5001 <plantsvzzombi@mail.ru>
2017-04-09 16:35:59 +0700

    upload

---
+++ app/index.php
@@ -0,0 +1,27 @@
+<!Doctype html>
+<html lang="ru">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="keywords" content=""/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/favicon.png" type="image/x-icon">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="css/mystyle.min.css">
+    <title></title>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+	
+<script type="text/javascript" src="script/scripts.min.js"></script>
+<!-- <script>window.jQuery || document.write('<script src="script/jquery.js"><\/script>')</script> -->
+</body>
+</html>
